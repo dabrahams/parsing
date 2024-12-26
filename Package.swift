fefd6b07e53b8d2306dd7e3f31c7d715ef7227cd
Dave Abrahams <dabrahams@adobe.com>
2024-12-25 19:55:54 -0800

    swift-collections, swift-graphs

--- Package.swift
+++ Package.swift
@@ -5,19 +5,30 @@ import PackageDescription
 
 let package = Package(
   name: "parsing",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "parsing",
             targets: ["parsing"]),
     ],
-    dependencies: [.package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")],
+    dependencies: [
+      .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
+      .package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
+      .package(url: "https://github.com/tevelee/swift-graphs.git", from: "0.2.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
           name: "parsing",
-          dependencies: [ .product(name: "Algorithms", package: "swift-algorithms") ]
+          dependencies: [
+            .product(name: "Algorithms", package: "swift-algorithms"),
+            .product(name: "Collections", package: "swift-collections"),
+            .product(name: "Graphs", package: "swift-graphs"),
+          ]
         ),
         .testTarget(
             name: "parsingTests",
